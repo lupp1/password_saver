6901b3024d42076a528350bd8e9d2ca8f5f08557
Gustavo <luppi_classic@protonmail.com>
2022-03-16 23:07:53 -0300

    Update 16/03

--- password_saver.py
+++ password_saver.py
@@ -1,15 +1,25 @@
+import PyQt5
+from Queries import *
 from getpass import getpass
 from mysql.connector import connect, Error
-from pass_queries import *
+
+insert_into_emails = """INSERT INTO emails(email, pass) VALUES ( 
+    {0},
+    {1}
+    )"""
+
+# TODO: Create tables, insert values, algorithm to insert and display using PyQT5
 
 try:
     with connect(
             host="localhost",
             user="root",
-            password=input('Insert password: '),
+            password=getpass("insert password: "),
             database="passwords_db") as connection:
         with connection.cursor() as cur:
-            pass
-
-except Error() as err:
+            # 1064 (42000) mySQL syntax error
+                cur.execute(insert_into_emails.format(str("test_email1@test.com"), str("potato33")))
+    
+except Exception as err:
     print(err)
+    
